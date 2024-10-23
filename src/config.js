d17f43fff423c2ab7cc2f83d4ad7cae22d96bc27
DisenoGRN <DisenoGRN@DISENO1GRN-PC>
2024-10-23 14:21:43 -0600

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 3000
+export const DB_HOST = process.env.DB_HOST || 'b7gneiyndkiuymhb7trz-mysql.services.clever-cloud.com'
+export const DB_DATABASE = process.env.DB_DATABASE || 'b7gneiyndkiuymhb7trz'
+export const DB_USER = process.env.DB_USER || 'umyvcol7dqgzmdec'
+export const  DB_PASSWORD = process.env.DB_PASSWORD || 'kU5T2SuH6yXyvdRYAV6z'
+export const DB_PORT = process.env.DB_PASSWORD || '3306'
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import expres from 'express'
+import {pool} from './db.js'
+import {} from './config.js'
+import bodyParser from 'body-parser'
+
+const app = expres()
+app.use(bodyParser.json())
+
+
+
+app.post('/validarcodigo', async (req, res) => {
+    try {
+        const { codigo } = req.body
+        const [result] = await pool.query('SELECT * FROM CodigoRegistro WHERE Codigo = ?', [codigo])
+
+        if (result.length > 0) {
+            res.json({ success: true, message: 'Código válido' })
+        } else {
+            res.json({ success: false, message: 'Código no encontrado' })
+        }
+    } catch (error) {
+        console.error('Error:', error);  // Log de errores para más detalles
+        res.status(500).json({ success: false, message: 'Error en el servidor', error: error.message })
+
+    }
+});
+
+app.get('/modelomonta', async (req, res) => {
+    try {
+      const [rows] = await pool.query('SELECT Modelo FROM Montacargas');
+      res.json(rows);
+    } catch (error) {
+      console.error('Error al consultar la base de datos:', error);
+      res.status(500).json({ message: 'Error en el servidor' });
+    }
+  });
+
+app.listen(3000)
+console.log('server')
